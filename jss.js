e87b9b36cdb42449a6da6bc9313f9492bacd1d4a
Anany814 <ananymishra180@gmail.com>
2026-01-23 12:14:35 +0530

    First Code

---
+++ jss.js
@@ -0,0 +1,40 @@
+// const container=document.querySelector(".container")
+// container.innerHTML="<h1>HTML </h1><p>This is created using JS</p>"
+// console.log(container)
+// .container{
+
+// function Show(){
+//     alert("This is an alert")
+// }
+// button.addEventListener("mouseover",Show)
+// const  btn=document.querySelector("#btn")
+// const  btn2=document.querySelector("#btn2")
+// function show()
+// {
+//     alert("You clicked the button")
+// }
+//     btn2.removeEventListener("click",show)
+//     btn.addEventListener("click",show)
+//     btn2.addEventListener("click",function(){
+//     btn.removeEventListener("click",show)
+//     })
+// const  btn=document.querySelector("#btn")
+// btn.addEventListener("keyup",(event)=>{
+//     event.preventDefault();
+//     console.log(event.srcElement[0].value)
+//     console.log(event.srcElement[1].value);
+//     console.log(event.key);
+// })
+//
+//calculate()
+
+//console.log("This is the beginning of the code")
+// setTimeout(()=>{console.log("After 2 sec")},2000)
+// console.log("This is the end of code")
+
+const setInterval(()=>{console.log("Printing at every two seconds")},2000)
+
+seTimeout(()=>{clearInterval(interval),1000})
+
+// const setInterval(()=>{console.log("Printing at every two seconds")},2000)
+// console.log(interval12)
